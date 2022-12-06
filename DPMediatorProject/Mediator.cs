68431a24a1a1c2a5c29bb25b17a5dcabd313eefe
efimov-top-academy <efimov_m@t.top-academy.ru>
2022-12-06 16:31:30 +0300

    Добавьте файлы проекта.

---
+++ DPMediatorProject/Mediator.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DPMediatorProject
+{
+    abstract class Mediator
+    {
+        public abstract void Message(string msg, Employe employe);
+    }
+
+    abstract class Employe
+    {
+        protected Mediator mediator;
+        public Employe(Mediator mediator)
+        {
+            this.mediator = mediator;
+        }
+        public virtual void Message(string msg)
+        {
+            mediator.Message(msg, this);
+        }
+
+        public abstract void Notify(string msg);
+    }
+
+    class Customer : Employe
+    {
+        public Customer(Mediator mediator) : base(mediator) { }
+
+        public override void Notify(string msg)
+        {
+            Console.WriteLine("To Customer: " + msg);
+        }
+    }
+
+    class Developer : Employe
+    {
+        public Developer(Mediator mediator) : base(mediator) { }
+
+        public override void Notify(string msg)
+        {
+            Console.WriteLine("To Developer: " + msg);
+        }
+    }
+
+    class Tester : Employe
+    {
+        public Tester(Mediator mediator) : base(mediator) { }
+
+        public override void Notify(string msg)
+        {
+            Console.WriteLine("To Tester: " + msg);
+        }
+    }
+
+    class Manager : Mediator
+    {
+        public Employe Customer { set; get; }
+        public Employe Developer { set; get; }
+        public Employe Tester { set; get; }
+
+        public override void Message(string msg, Employe employe)
+        {
+            
+            if(employe == Customer)
+                Developer.Notify(msg);
+            if(employe == Developer)
+            {
+                Customer.Notify(msg);
+                Tester.Notify(msg);
+            }
+            if(employe == Tester)
+            {
+                Customer.Notify(msg);
+                Developer.Notify(msg);
+            }
+            
+        }
+
+    }
+}
---
+++ DPMediatorProject/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace DPMediatorProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Manager manager = new();
+            Employe customer = new Customer(manager);
+            Employe developer = new Developer(manager);
+            Employe tester = new Tester(manager);
+
+            manager.Customer = customer;
+            manager.Developer = developer;
+            manager.Tester = tester;
+
+            customer.Message("Order! Program for my bank");
+            developer.Message("First Step finish. Testing");
+            tester.Message("All right. Prog is good");
+        }
+    }
+}
